571f82bd30efd288ee7cf11d737104c4a0aa9043
Satyam singh <87685197+mrsingh2324@users.noreply.github.com>
2023-04-25 18:45:14 +0530

    updated signin

--- pages/components/signin.js
+++ pages/components/signin.js
@@ -1,66 +1,72 @@
 import Head from "next/head";
-// import styles from "@/styles/Signup.module.css";
+import styles from "@/styles/Home.module.css";
 import Image from "next/image";
-import apple from '../public/apple.png';
-import google from '../public/google.png';
-import { useRouter } from "next/router";
+import apple from '../../public/apple.png';
+import google from '../../public/google.png';
+
 
 import { useSession, signIn, signOut, getSession } from "next-auth/react" //detect user is present or not
 import { useEffect, useState } from "react";
 
-export default function Signin({session}) {
-    const router = useRouter()
+export default function Home({session}) {
+  
+  
   
   console.log(session);
 
   return (
-    <div className="signup flex h-screen overflow-hidden">
+    <div className="home">
       <div className="name">
         <h1>Board.</h1>
       </div>
-      <div className="authenticate h-screen ">
+      <div className="authenticate ">
         <div className="content">
-          <h2>Sign up</h2>
-          <span className="span-tag">Create your account</span>
+        <h1 className="flex justify-center gap-5 items-center mt-2 italic bg-white rounded-lg p-2">
+            Got to dashboard without loggin in{" "}
+            <button
+              className="bg-blue-600 rounded p-1 font-bold "
+              onClick={() => router.push("/dashboard")}
+            >
+              continue
+            </button>
+          </h1>
+          <h2>Sign in</h2>
+          <span className="span-tag">Sign in to your account</span>
           <div className="social">
             <div>
+
               <a>
                 <button className="hover:bg-blue-500 hover:text-white" onClick={()=>signIn()}>
                 <Image src={google} alt="google" />
-                <span>Sign up with Google</span>
+                <span>Sign in with Google</span>
                 </button>
                 </a>
             </div>
             <div>
               <button className="hover:bg-blue-500 hover:text-white" >
                 <Image src={apple} alt="apple" />
-                <span>Sign up with Apple</span>
+                <span>Sign in with Apple</span>
               </button>
             </div>
           </div>
           <div className="form">
             <form>
               <div>
-                <label htmlFor="">Full name</label>
-                <input type="text" placeholder="Full name" />
-              </div>
-              <div>
                 <label htmlFor="">Email address</label>
-                <input type="email" placeholder="Email address" />
+                <input type="email" placeholder="Username" />
               </div>
               <div>
                 <label htmlFor="">Password</label>
                 <input type="password" placeholder="Password" />
               </div>
-              <div>
-                <label htmlFor="">Confirm password</label>
-                <input type="password" placeholder="Confirm password" />
-              </div>
-              <button className="sign-up-btn">Sign up</button>
+              <a href="" className="forget-pass">
+                Forgot password?
+              </a>
+              <button className="sign-in-btn">Sign in</button>
             </form>
           </div>
-          <p className="login-link">
-            Already have an account? <a href="" onClick={router.push('/')}>Login here</a>
+          <p className="register-link">
+            Dont't have an account? <a href="">Register here</a>
           </p>
         </div>
       </div>
@@ -85,3 +91,5 @@ export async function getServerSideProps({ req }) {
     },
   };
 }
+
+
